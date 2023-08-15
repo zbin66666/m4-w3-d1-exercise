5fba71bda2a858139d9935fa120dc2997cd20b1f
Bin Zhang <binzhang@Bins-MacBook-Pro.local>
2023-08-15 15:22:49 -0700

    complete

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default class MyComponent extends React.Component {
   render() {
-    return <div />;
+    return <div><input
+    onChange={(event) => {this.setState({input: event.target.value})}}
+    type="text" /></div>;
   }
 }
--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -6,4 +6,32 @@ describe("MyComponent", () => {
   it("should render correctly", () => {
     const component = shallow(<MyComponent />);
   });
+  
+
+    expect(component.getElements()).toMatchSnapshot();
+  });
+  it("should create an entry in component state", () =>{
+    
+    const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    // when
+    form.props().onChange({target:{
+      name:'myName',
+      value:"myValue"
+    }});
+    // then
+    expect(component.state('input')).toBeDefined();
+});
+
+it("should create an entry in component state with the event value", () =>{
+    
+  const component = shallow(<MyComponent />);
+  const form = component.find('input');
+  // when
+  form.props().onChange({target:{
+    name:'myName',
+    value:"myValue"
+  }});
+  // then
+  expect(component.state('input')).toBeDefined();
 });
